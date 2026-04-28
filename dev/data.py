c7ffab5e863b58325d8652fa5c53bca2bffb88c9
Solomiya_Les <solomiya.lesivtsiv@gmail.com>
2026-04-28 14:11:12 +0300

    - Wire: implemented propagation mechanism via @property.setter; added destinations list for registering connected elements. - Gate: implemented automatic gate registration in input conductors and update() method for instant response to signal change. - Logic Gates: rewritten evaluate() methods to use pure boolean values ​​(bool(status)), which eliminates type errors. - Added Switch and OUT classes for convenient control and monitoring of the circuit state. - Fixed Copy, Out and Switch classes for correct signal branching to multiple lines.

--- dev/data.py
+++ dev/data.py
@@ -48,10 +48,26 @@ class Wire:
     """
 
     def __init__(self):
-        self.status: Signal = None
+        self._status = Signal(False)
+        # For propagation
+        self.destinations = []
         # self.start = i
         # self.end = o
+    @property
+    def status(self):
+        return self._status
 
+    @status.setter
+    def status(self, new_signal):
+        # If the signal has not changed, we do nothing
+        if self._status == new_signal:
+            return
+
+        self._status = new_signal
+
+        # Notify all connected gates that the signal has changed
+        for gate in self.destinations:
+            gate.update()
 class Gate:
 
     """
@@ -59,10 +75,19 @@ class Gate:
     """
 
     def __init__(self, w_in: list, w_out = None):
-
+        # Save a reference to the input wires(list of objects)
         self.input_w = w_in
+        # Save a reference to the output wire where will pass the result
         self.output_w = w_out
 
+        # Register this gate in the input conductors
+        # Now, when the signal changes on any of these Wires, it will know
+        # to trigger an update on that particular gate.
+        for wire in self.input_w:
+            wire.destinations.append(self)
+        # Perform the initial logic gate evaluation (evaluate)
+        # and write the result to the output conductor status (output_w.status).
+        # We also store a copy of the result in self.out for internal use.
         self.output_w.status = self.out = self.evaluate()
 
     def __repr__(self):
@@ -76,6 +101,15 @@ class Gate:
 
         return Signal(False)
 
+    def update(self):
+        """
+        The method that is called by the conductor when the signal changes
+        """
+        
+        new_val = self.evaluate()
+        # We update the output conductor - this will start the next wave of propagation
+        self.output_w.status = new_val
+
 class Switch:
 
     """
@@ -83,10 +117,35 @@ class Switch:
     """
 
     def __init__(self, value = False, wire = None):
-        self.status = Signal(value)
+        self._status = Signal(value)
         self.wire = wire
-        self.wire.status = self.status
+        # Initialize the conductor with an initial value
+        if self.wire:
+            self.wire.status = self._status
+    def turn_on(self):
+        """
+        Turn on a Signal(True)
+        """
+        self._status = Signal(True)
+        if self.wire:
+            self.wire.status = self._status
 
+    def turn_off(self):
+        """
+        Turns off a Signal(False)
+        """
+        self._status = Signal(False)
+        if self.wire:
+            self.wire.status = self._status
+
+    def flip(self):
+        """
+        Flip to opposite status
+        """
+        new_val = not self._status.value
+        self._status = Signal(new_val)
+        if self.wire:
+            self.wire.status = self._status
 class OUT:
 
     """
@@ -96,7 +155,10 @@ class OUT:
     def __init__(self, wire = None):
         self.wire = wire
         self.status = self.wire.status
-
+    @property
+    def status(self):
+        # Always returns the current state of the conductor it is connected to
+        return self.wire.status if self.wire else Signal(False)
     def __repr__(self):
         return f"output={self.status}"
 
@@ -107,11 +169,25 @@ class COPY:
     """
 
     def __init__(self, wire = None, wires: list = None):
-        self.wire = wire
-        self.wires = wires
+        self.input_w = [wire]
+        self.outputs = wires
 
-        for w in self.wires:
-            w.status = self.wire.status
+        wire.destinations.append(self)
+        self.update()
+
+    def update(self):
+        """
+        The method that is called by the conductor when the signal changes
+        """
+        val = self.evaluate()
+        for w in self.outputs:
+            w.status = val
+
+    def evaluate(self):
+        """
+        Evaluate output signal value
+        """
+        return self.input_w[0].status
 
 class NOT(Gate):
 
@@ -125,8 +201,8 @@ class NOT(Gate):
         Evaluate output signal value
         """
 
-        wire = self.input_w[0]
-        return Signal(not wire.status)
+        wire = bool(self.input_w[0].status)
+        return Signal(not wire)
 
 class AND(Gate):
 
@@ -140,8 +216,9 @@ class AND(Gate):
         Evaluate output signal value
         """
 
-        wire_1, wire_2 = self.input_w
-        result = wire_1.status and wire_2.status
+        wire_1 = bool(self.input_w[0].status)
+        wire_2 = bool(self.input_w[1].status)
+        result = bool(wire_1 and wire_2)
         return Signal(result)
 
 class NAND(Gate):
@@ -156,8 +233,9 @@ class NAND(Gate):
         Evaluate output signal value
         """
 
-        wire_1, wire_2 = self.input_w
-        result = wire_1.status and wire_2.status
+        wire_1 = bool(self.input_w[0].status)
+        wire_2 = bool(self.input_w[1].status)
+        result = wire_1 and wire_2
         return Signal(not result)
 
 class OR(Gate):
@@ -172,8 +250,9 @@ class OR(Gate):
         Evaluate output signal value
         """
 
-        wire_1, wire_2 = self.input_w
-        result = wire_1.status or wire_2.status
+        wire_1 = bool(self.input_w[0].status)
+        wire_2 = bool(self.input_w[1].status)
+        result = wire_1 or wire_2
         return Signal(result)
 
 class NOR(Gate):
@@ -188,8 +267,9 @@ class NOR(Gate):
         Evaluate output signal value
         """
 
-        wire_1, wire_2 = self.input_w
-        result = wire_1.status or wire_2.status
+        wire_1 = bool(self.input_w[0].status)
+        wire_2 = bool(self.input_w[1].status)
+        result = wire_1 or wire_2
         return Signal(not result)
 
 class XOR(Gate):
@@ -204,8 +284,9 @@ class XOR(Gate):
         Evaluate output signal value
         """
 
-        wire_1, wire_2 = self.input_w
-        result = (wire_1.status or wire_2.status) and (not wire_1.status or not wire_2.status)
+        wire_1 = bool(self.input_w[0].status)
+        wire_2 = bool(self.input_w[1].status)
+        result = (wire_1 or wire_2) and (not wire_1 or not wire_2)
         return Signal(result)
 
 class XNOR(Gate):
@@ -220,8 +301,9 @@ class XNOR(Gate):
         Evaluate output signal value
         """
 
-        wire_1, wire_2 = self.input_w
-        result = (wire_1.status or wire_2.status) and (not wire_1.status or not wire_2.status)
+        wire_1 = bool(self.input_w[0].status)
+        wire_2 = bool(self.input_w[1].status)
+        result = (wire_1 or wire_2) and (not wire_1 or not wire_2)
         return Signal(not result)
 
 w1, w2, w3 = Wire(), Wire(), Wire()
