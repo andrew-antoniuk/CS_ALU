61ad451bb327fe16e6f7bf9f294c4a79d5393754
andrew-antoniuk <a.antoniuk.pn@ucu.edu.ua>
2026-05-08 03:13:09 +0300

    File moves

---
+++ dev/interface.py
@@ -0,0 +1,26 @@
+import sys
+import os
+from PyQt5 import QtWidgets, uic, QtGui
+
+class MyWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(MyWindow, self).__init__()
+
+        # 1. АВТОМАТИЧНИЙ ПОШУК ШЛЯХУ
+        # Отримуємо шлях до папки, де лежить цей файл (interface.py)
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        # Приєднуємо назву файлу інтерфейсу
+        ui_path = os.path.join(current_dir, 'interface.ui')
+
+        # Завантажуємо інтерфейс
+        try:
+            uic.loadUi(ui_path, self)
+        except FileNotFoundError:
+            print(f"ПОМИЛКА: Файл не знайдено за шляхом: {ui_path}")
+            return
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    window = MyWindow()
+    window.show()
+    sys.exit(app.exec_())
---
+++ dev/main.py
@@ -0,0 +1,65 @@
+"""
+Launch
+"""
+
+import sys
+
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5 import QtGui
+from code import Ui_Simulator
+from items import CircuitScene
+
+class SimulatorWindow(QMainWindow):
+
+    def __init__(self):
+
+        super().__init__()
+
+        self.ui = Ui_Simulator()
+
+        self.ui.setupUi(self)
+
+        self.scene = CircuitScene()
+
+        self.ui.graphicsView.setScene(self.scene)
+        self.scene.setSceneRect(0, 0, 3000, 3000)
+        self.ui.graphicsView.setRenderHint(QtGui.QPainter.Antialiasing)
+        self.ui.graphicsView.setDragMode(self.ui.graphicsView.RubberBandDrag)
+
+        self.ui.listWidget.itemClicked.connect(
+            self.select_tool
+        )
+
+        self.ui.actionCursor.triggered.connect(
+            self.reset_tool
+        )
+
+    def select_tool(self, item):
+
+        text = item.text()
+
+        if text == "AND Gate":
+            self.scene.current_tool = "AND"
+
+        elif text == "OR Gate":
+            self.scene.current_tool = "OR"
+
+        elif text == "Switch":
+            self.scene.current_tool = "SWITCH"
+
+        elif text == "Bulb":
+            self.scene.current_tool = "LED"
+
+    def reset_tool(self):
+
+        self.scene.current_tool = None
+
+if __name__ == "__main__":
+
+    app = QApplication(sys.argv)
+
+    window = SimulatorWindow()
+
+    window.show()
+
+    sys.exit(app.exec_())
